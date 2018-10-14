2ec5bdbbbcddcd9d2c79698e0703503dc0b135ae
Sani Mesic <dat14sme@student.lu.se>
2018-10-14 14:40:50 +0200

    getRides and getAllTravelers

--- server/src/main/java/se/lth/base/server/data/RideDataAccess.java
+++ server/src/main/java/se/lth/base/server/data/RideDataAccess.java
@@ -3,6 +3,7 @@ package se.lth.base.server.data;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -16,12 +17,9 @@ import se.lth.base.server.database.Mapper;
  * @author Sani Mesic
  * 
  * 
- * TODO oklart om Location kan användas som parameter då koordinaterna är i en annan table
- * 
  */
 public class RideDataAccess extends DataAccess<Ride> {
-
-	private final LocationDataAccess locationDao = new LocationDataAccess(Config.instance().getDatabaseDriver());
+	private final RidePersonDataAccess ridePersonDao = new RidePersonDataAccess(Config.instance().getDatabaseDriver());
 	private final UserDataAccess userDao = new UserDataAccess(Config.instance().getDatabaseDriver());
 	
 	private static class RideMapper implements Mapper<Ride> {
@@ -32,7 +30,8 @@ public class RideDataAccess extends DataAccess<Ride> {
 					resultSet.getString("departure_time"), 
 					resultSet.getString("arrival_time"),
 					resultSet.getInt("nbr_seats"),
-					resultSet.getInt("driver_id"));
+					resultSet.getInt("driver_id"),
+					"Undefined");
 		}
 	}
 
@@ -53,7 +52,7 @@ public class RideDataAccess extends DataAccess<Ride> {
 	public Ride createRide(String departureLocation, String destination, String departureTime, String arrivalTime, int nbrSeats, int driverId) {
 		int rideId = insert("INSERT INTO rides (departure_time, arrival_time, nbr_seats, driver_id, departure_location, destination) VALUES (?,?,?,?,?,?)",
 				        departureTime, arrivalTime, nbrSeats, driverId, departureLocation, destination);
-		return new Ride(rideId, departureLocation, destination, departureTime, arrivalTime, nbrSeats, driverId);
+		return new Ride(rideId, departureLocation, destination, departureTime, arrivalTime, nbrSeats, driverId, "Undefined");
 	}
 	
 	/**
@@ -73,18 +72,29 @@ public class RideDataAccess extends DataAccess<Ride> {
 	public Ride getRide(int rideId) {
 		return queryFirst("SELECT * FROM rides WHERE ride_id = ?", rideId);
 	}
-	
+		
 	/**
 	 * @param userId
 	 * @return all rides connected to one user id, i.e. where the specific user is either a passenger or driver
 	 *
-	 *
-	 * TODO only gets rides where the user is a driver
-	 * DOESNT WORK COMPLETELY 
 	 */
 	public List<Ride> getRides(int userId) {
-		return query("SELECT * FROM rides JOIN ride_passengers ON rides.driver_id = ride_passengers.user_id WHERE user_id = ?", userId);
+		List<RidePerson> ridesAsPassenger = ridePersonDao.getRidesAsPassenger(userId);
+		
+		// Insert rides as driver directly, set role to driver
+		List<Ride> rides =  query("SELECT * FROM rides WHERE driver_id = ?", userId);
+		for (int i = 0; i < rides.size(); i++) {
+			rides.get(i).setRole("Driver");
+		}
+		// Insert passengers
+		for (int i = 0; i < ridesAsPassenger.size(); i++) {
+			Ride ride = queryFirst("SELECT * FROM rides WHERE ride_id = ?", ridesAsPassenger.get(i).getRideId());
+			ride.setRole("Passenger");
+			rides.add(ride);
+		}
+		return rides;
 	}
+	
 	
 	/**
 	 * 
